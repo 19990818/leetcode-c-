5d079bf5db11737b7d00c1a8e0cde77d3b126972
19990818 <2013060018@qq.com>
2022-09-30 21:37:13 +0800

    leetcode-2022-9-30

--- 2022-9-29.cpp
+++ 2022-9-29.cpp
@@ -1,5 +1,7 @@
 #include<string>
 #include <vector>
+#include <algorithm>
+#include <functional>
 using namespace std;
 
 class Solution {
@@ -43,6 +45,33 @@ public:
 class Solution {
 public:
     vector<int> advantageCount(vector<int>& nums1, vector<int>& nums2) {
+       sort(nums1.begin(),nums1.end());
+       vector<pair<int,int>> num2;
+        for(int i=0;i<nums2.size();i++){
+            num2.push_back(make_pair(nums2[i],i));
+        }
+        // function<bool(int,int)>less=[&](int i,int j){
+        //     return i<j;
+        // };
+        vector<int> ans(nums1.size(),-1);
+        //sort(nums1.begin(),nums1.end(),less);
+        sort(num2.begin(),num2.end());
 
+        int i=nums1.size()-1,j=nums1.size()-1;
+        for (;i>=0;){
+            for (;i>=0&&nums1[j]<=num2[i].first;){
+                --i;
+            }
+            if(i<0)break;
+            ans[num2[i].second]=nums1[j];
+            --i,--j;
+        }
+        for(int k=0;k<nums1.size();k++){
+            if(ans[k]==-1){
+                ans[k]=nums1[j];
+                j--;
+            }
+        }
+        return ans;
     }
 };
